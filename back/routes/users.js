8be50c566785559246026e966facb96158e15f93
antipaeorg1 <antipaeorg1@hotmail.com>
2023-10-31 14:01:01 +0200

    Register and Login MVP added

--- back/routes/users.js
+++ back/routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController')
 
 // router.get('/users', userController.getUserList);
-// router.get('/users/:id', userController.getUserDetails);
 // router.post('users/:id/delete', userController.deleteUser);
 
 module.exports = router;
